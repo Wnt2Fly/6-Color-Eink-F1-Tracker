553e89ac72104c042e9953a60dc78a9c1e77a488
Wnt2Fly <tmccartn@gmail.com>
2026-05-27 14:23:32 -0400

    Updated first

--- images/Windows/converterTo6color_all.cmd
+++
@@ -1,4 +0,0 @@
-for %%i in (*.bmp) do convert.exe %%i
-for %%i in (*.jpg) do convert.exe %%i
-for %%i in (*.jpeg) do convert.exe %%i
-for %%i in (*.png) do convert.exe %%i
--- tools/trace_decode.py
+++
@@ -1,4 +0,0 @@
-"""Trace u8g2 decode steps for glyph 'l' in f1bold10."""
-from patch_f1_font import extract_font_bytes, read_font_info, find_glyph_blob, decode_glyph_verbose
-
-# Will inline if import fails — copy minimal from patch script
